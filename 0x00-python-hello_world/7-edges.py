8b6f1f351302e1c49970762a65c3efd22b26b7f1
Ibukun Ogwu <ijogwu@yahoo.com>
2022-11-28 08:11:51 -0800

    Hello World

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = f"{word[:3]}"
+word_last_2 = f"{word[-2:]}"
+middle_word = f"{1:-1]}"
+print(f"First three letters: {word_first_3}")
+print(f"Last two letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
