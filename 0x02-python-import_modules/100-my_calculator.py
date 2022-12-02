293c272967970eb63ae3d207142224b34b091ffc
Ibukun Ogwu <ijogwu@yahoo.com>
2022-12-01 16:36:07 -0800

    New

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-        dic = {"+": add, "-": sub, "*": mul, "/": div}
-        if sys.argv[2] not in list(dic.keys()):
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+    dic = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(dic.keys()):
+       print("Unknown operator. Available operators: +, -, * and /")
+       sys.exit(1)
 
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            print("{} {} {} = {}".format(a, sys.argv[2], b, dic[sys.argv[2]](a, b)))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, dic[sys.argv[2]](a, b)))
