2b5a34004244450c3d43b1b94de4a00b62b827e7
Ibukun Ogwu <ijogwu@yahoo.com>
2023-01-10 03:26:17 -0800

    Python Input Output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,5 +3,7 @@
 
 
 def class_to_json(obj):
-    """Return the dictionary representation of a simple data structure."""
+    """Return the dictionary representation of a simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serilalization of an object """
     return obj.__dict___
