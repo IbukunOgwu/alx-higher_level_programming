ecba52f66591d767dcf232c4e0201d27b66608ea
Ibukun Ogwu <ijogwu@yahoo.com>
2023-02-14 14:48:47 -0800

    MySQL

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,5 @@
---Displays the max temperature of each state, ordered by state name.
+--Displays the max temperature of each state.
+-- This is ordered by state name.
 SELECT 'state', MAX('value') AS 'max_temp'
 FROM 'temperatures'
 GROUP BY 'state'
