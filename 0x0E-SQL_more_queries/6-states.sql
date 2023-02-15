fe23a960986b981bb439adbf32d2723114df1dbb
Ibukun Ogwu <ijogwu@yahoo.com>
2023-02-15 03:41:52 -0800

    SQL

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
--- Creates the table hbtn_0d_usa with table States.
+-- Creates the table hbtn_0d_usa and table States on the MySQL server
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'hbtn_od_usa'.'states' (
-	PRIMARY KEY('id'),
-	'id' INT            NOT NULL AUTO_INCREMENT,
-	'name' VARCHAR(256) NOT NULL
-);
+-- Use a database
+USE hbtn_0d_usa;
+-- Creates the table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
