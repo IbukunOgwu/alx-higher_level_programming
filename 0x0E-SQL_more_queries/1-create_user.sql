db8855dd2838359ea06fe2110a56c1a98c1aa7b3
Ibukun Ogwu <ijogwu@yahoo.com>
2023-02-15 03:07:17 -0800

    SQL

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates MySQL server user user_0d_1.
+-- The user should have all privileges on MySQL server.
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost
