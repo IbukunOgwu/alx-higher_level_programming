afdc82f19071e94be30c29ee91ce05a7eb8660b9
root <ijogwu@yahoo.com>
2023-03-18 22:53:36 +0000

    ORM

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This  gets the state.id from a city  """
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True
+            )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    city = session.query(
+        State
+        ).filter(
+            State.name.like('%s' % (sys.argv[4], ))
+            ).first()
+    if city is None:
+        print("Not found")
+    else:
+        print(city.id)
