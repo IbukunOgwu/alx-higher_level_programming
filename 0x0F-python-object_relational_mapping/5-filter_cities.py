6348059490cb42880c11ef48af527d12da7bbf00
root <ijogwu@yahoo.com>
2023-03-18 23:20:29 +0000

    ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""This lists all states with a name starting
-with N (upper N) from the database hbtn_0e_4_usa
+"""This displays all cities of a given state from the
+ states table of the database hbtn_0e_4_usa.
+ Safe from SQL injections.
+ Usage: ./5-filter_cities.py <mysql username> \
+                             <mysql password> \
+                             <database name> \
+                             <state name searched>
 """
-import MySQLdb
 import sys
-if __name__ == "__main__":
+import MySQLdb
 
-    db = MySQLdb.connect(
-        host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor_db = db.cursor()
-    num_cities = cursor_db.execute(
-        "SELECT name FROM cities\
-        WHERE state_id = (SELECT id FROM states WHERE name = %s)\
-        ORDER BY cities.id ASC;", (sys.argv[4], )
-    )
-    cities = cursor_db.fetchall()
-    for city in range(0, num_cities):
-        if city is not 0:
-            print(", ", end="")
-        print(cities[city][0], end="")
-    print('')
-    cursor_db.close()
-    db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
