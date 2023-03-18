ad74c2cf0ea3b7476866f7b15f36753b8d7d67db
root <ijogwu@yahoo.com>
2023-03-18 22:40:00 +0000

    ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa
+with N (upper N) from the database hbtn_0e_4_usa
 """
 import MySQLdb
 import sys
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" state class module """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ MySQL states table representation
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    name = Column(String(length=128), nullable=False)
+
+    def __init__(self, name, id=None):
+        """ State constructor """
+        self.id = None
+        self.name = name
